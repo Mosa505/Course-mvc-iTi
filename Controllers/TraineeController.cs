5e971e36f0fa61d28eadb48b361a43dfdd57a63b
Mosa <Mohamedmosa6000@gmail.com>
2025-07-31 23:48:59 +0300

    add TraineeController Actian Details

---
+++ Controllers/TraineeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Course_mvc_iTi.Controllers
+{
+    public class TraineeController : Controller
+    {
+        public IActionResult Details()
+        {
+
+            return View();
+        }
+    }
+}
