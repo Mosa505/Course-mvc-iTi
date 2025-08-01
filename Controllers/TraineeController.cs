3fdacd9cecb3986b8c43cd2ba68a58d663f4d289
Mosa <Mohamedmosa6000@gmail.com>
2025-08-01 03:00:28 +0300

    First Try Assignment 3

--- Controllers/TraineeController.cs
+++ Controllers/TraineeController.cs
@@ -1,13 +1,45 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Course_mvc_iTi.Models;
+using Course_mvc_iTi.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Course_mvc_iTi.Controllers
 {
     public class TraineeController : Controller
     {
-        public IActionResult Details()
+        public CourseDbContext context = new CourseDbContext();
+        public IActionResult Details(int id)
         {
+            var TraineId = context.trainees.FirstOrDefault(x => x.Id == id);
+            var TraineGrade = context.trainees.Select(x => x.Grade).ToList();
+            var CosName = context.courses.Select(x => x.Name).ToList();
+          
+            string ColorRed = "Red";
+            string ColorGreen = "Green";
+            DetailsTraineeScoreViewModel Det = new DetailsTraineeScoreViewModel();
 
-            return View();
+            Det.TraineeId = TraineId.Id;
+            Det.TraineeName = TraineId.Name;
+            foreach (var Iteam in CosName)
+            {
+                Det.CourseName = Iteam;
+            }
+            foreach (var Iteam in TraineGrade)
+            {
+                 Det.Degree =(int)Iteam;
+            
+            }
+           
+
+            if (Det.Degree > 60)
+            {
+                Det.Color= ColorGreen;
+            }
+            else
+                Det.Color = ColorRed;
+
+
+            return View(Det);
         }
     }
 }
---
+++ ViewModels/DetailsTraineeScoreViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Course_mvc_iTi.ViewModels
+{
+    public class DetailsTraineeScoreViewModel
+    {
+        public int TraineeId { get; set; }
+        public string TraineeName { get; set; }
+        public string CourseName { get; set; }
+        public string Color{ get; set; }
+        public int Degree { get; set; }
+    }
+}
