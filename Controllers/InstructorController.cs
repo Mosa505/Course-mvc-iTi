72a0db5971b13a88bf161be9c373e1860ae473c0
Mosa <Mohamedmosa6000@gmail.com>
2025-08-19 20:08:31 +0300

    Add Edit Property

--- Controllers/InstructorController.cs
+++ Controllers/InstructorController.cs
@@ -19,5 +19,40 @@ namespace Course_mvc_iTi.Controllers
 
             return View(ViewById);
         }
+
+        public IActionResult Edit(int id)
+        {
+            ViewData["DeptId"] = context.departments.ToList();
+            ViewData["CourseId"] = context.courses.ToList();
+            return View(context.instructors.FirstOrDefault(E => E.Id == id));
+
+        }
+
+        public IActionResult SaveEdit(int id, Instructor Inst)
+        {
+            Instructor OldData = context.instructors.FirstOrDefault(e => e.Id == id);
+
+            if (Inst.Name != null && Inst.Salary != null)
+            {
+                
+                if (OldData != null)
+                {
+                    OldData.Name = Inst.Name;
+                    OldData.Salary = Inst.Salary;
+                    OldData.Imag = Inst.Imag;
+                    OldData.Address = Inst.Address;
+                    OldData.Crs_Id = Inst.Crs_Id;
+                    OldData.Dept_Id = Inst.Dept_Id;
+                    context.SaveChanges();
+                    return RedirectToAction("Index");
+
+                }
+            }
+            ViewData["DeptId"] = context.departments.ToList();
+            ViewData["CourseId"] = context.courses.ToList();
+            return View("Edit" , Inst);
+
+
+        }
     }
 }
